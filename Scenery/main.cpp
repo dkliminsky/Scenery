894110682676145b8ae06991c3858fb1320176d8
turlicht <turlicht@yandex.ru>
2014-11-12 13:13:20 +0300

    vs project

--- Scenery/main.cpp
+++ Scenery/main.cpp
@@ -5,12 +5,12 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    QCoreApplication::setOrganizationName("turlicht");
-    QCoreApplication::setOrganizationDomain("turlicht.tk");
-    QCoreApplication::setApplicationName("Scenery");
+	QApplication a(argc, argv);
+	QCoreApplication::setOrganizationName("turlicht");
+	QCoreApplication::setOrganizationDomain("turlicht.tk");
+	QCoreApplication::setApplicationName("Scenery");
 
-    MainWindow mainWindow(nullptr);
-    mainWindow.show();
-    return a.exec();
+	MainWindow mainWindow(nullptr);
+	mainWindow.show();
+	return a.exec();
 }
