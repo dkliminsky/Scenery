b88c505ba3994afeec93e011db5ccaa72a2dcd8b
Dmitry Kliminsky <turlicht@yandex.ru>
2014-11-14 14:30:53 +0300

    first fix gui links

--- Scenery/gui/linkitem.cpp
+++ Scenery/gui/linkitem.cpp
@@ -21,7 +21,8 @@ LinkItem::LinkItem(Node *node_from, Node *node_to) :
 QRectF LinkItem::boundingRect() const
 {
     QRect rect = calcRect();
-    return QRectF(- rect.width() / 2, - rect.height() / 2, rect.width() / 2, rect.height());
+    return QRectF(-rect.width() / 2, -rect.height() / 2,
+                   rect.width() / 2,  rect.height() / 2);
 }
 
 void LinkItem::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
@@ -30,15 +31,27 @@ void LinkItem::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, 
     Q_UNUSED(widget);
 
     QRect rect = calcRect();
-    painter->drawLine(- rect.width() / 2, - rect.height() / 2, rect.width() / 2, rect.height());
+    if (node_from->posY() <= node_to->posY()) {
+        painter->drawLine(-rect.width() / 2, -rect.height() / 2,
+                           rect.width() / 2,  rect.height() / 2);
+    }
+    else {
+        painter->drawLine(-rect.width() / 2,  rect.height() / 2,
+                           rect.width() / 2, -rect.height() / 2);
+    }
 }
 
 void LinkItem::repaint()
 {
+    prepareGeometryChange();
     QRect rect = calcRect();
     setPos(rect.x() + rect.width() / 2,
            rect.y() + rect.height() / 2);
-    update(0, 0, rect.width(), rect.height());
+//    update(-rect.width() / 2, -rect.height() / 2,
+//            rect.width() / 2,  rect.height() / 2);
+//    update(rect.x(), rect.y(),
+//           rect.x() + 200, rect.y() + 200);
+    //update(0, 0, rect.width(), rect.height());
 }
 
 QRect LinkItem::calcRect() const
